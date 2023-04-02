4ef5de1f49d42c3c92c17b1ceb89de0bcdab96f5
pkar <pkar@ieee.org>
2023-04-02 10:23:52 +0200

    feat: GetEmoji - creates Pidgin emoji theme

---
+++ GetEmojis/Module1.vb
@@ -0,0 +1,101 @@
+﻿Imports System.Net.Http
+Imports HtmlAgilityPack
+
+Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("GetEmojis")
+
+        Dim targetDir As String = IO.Path.Combine(Environment.CurrentDirectory, "unicode")
+        Dim themeFile As String = IO.Path.Combine(targetDir, "theme")
+
+        'If IO.Directory.Exists(targetDir) Then
+        '    Console.WriteLine("Folder Unicode already exists, exiting")
+        '    Exit Sub
+        'End If
+
+
+        IO.Directory.CreateDirectory(targetDir)
+
+        ThemeFileHeader(themeFile)        ' oficjalne 
+        ' GetOfficialEmojis(targetDir)
+
+        ' z pomijaniem już istniejących
+        GetProposedEmojis(targetDir, themeFile)
+
+    End Sub
+
+    Private Sub ThemeFileHeader(themeFile As String)
+
+        IO.File.Delete(themeFile)
+        IO.File.WriteAllText(themeFile,
+$"Name=Unicode {Date.Now.ToString("yyyyMMdd")} 
+Description=Emoji from Unicode data file
+Icon=1fae2.png
+Author=pkar
+
+[default]
+"
+)
+    End Sub
+
+    Private _http As New HttpClient
+
+    Private Sub GetOfficialEmojis(targetDir As String)
+        ' już istniejące Emoji
+
+        Dim sPage As String = _http.GetStringAsync("http://www.unicode.org/emoji/charts/emoji-list.html").Result
+
+    End Sub
+
+    Private Sub GetProposedEmojis(targetDir As String, themeFile As String)
+        ' propozycje, ale na liście są też oficjalne (niektóre?), więc je pomijamy... a może w ogóle wszystkie tu są?
+
+        Dim sPage As String = _http.GetStringAsync("http://www.unicode.org/emoji/charts/emoji-proposals.html").Result
+
+        If String.IsNullOrWhiteSpace(sPage) Then Return
+
+        Dim oHtml As New HtmlDocument()
+        oHtml.LoadHtml(sPage)
+
+        Dim oTable As HtmlNode = oHtml.DocumentNode.SelectNodes("//table")(1)
+
+        Dim counter As Integer = 0
+
+        For Each oRow As HtmlNode In oTable.SelectNodes("tr")
+            Dim oRows = oRow.SelectNodes("td")
+            If oRows Is Nothing Then Continue For
+
+            Dim emoji As String = oRows(0).InnerText
+
+            Dim sFilename As String = emoji.Replace(" ", "-").ToLower & ".png"
+
+            Dim sIcon = oRows(1).SelectSingleNode("img").Attributes("src").Value
+            ' data:image/png;base64,zzzzz....
+
+            If Not sIcon.StartsWith("data:image/png;base64,") Then
+                Console.WriteLine("Inny img src dla " & sFilename)
+            Else
+                Dim decoded = System.Convert.FromBase64String(sIcon.Substring("data:image/png;base64,".Length))
+                IO.File.WriteAllBytes(IO.Path.Combine(targetDir, sFilename), decoded)
+
+                Dim sLinia As String = sFilename & vbTab
+                Dim codepoints = emoji.Split(" ")
+                For Each cdpn In codepoints
+                    Dim utf32 As Int32 = Int32.Parse(cdpn, Globalization.NumberStyles.AllowHexSpecifier)
+                    sLinia &= Char.ConvertFromUtf32(utf32)
+                Next
+
+                IO.File.AppendAllText(themeFile, sLinia & vbCrLf)
+
+                counter += 1
+            End If
+
+        Next
+
+        Console.WriteLine("Emojis count: " & counter)
+
+    End Sub
+
+End Module
---
+++ GetEmojis/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GetEmojis")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("GetEmojis")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("4984dda6-bab7-4e49-9231-1a239641de69")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
